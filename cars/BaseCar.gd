72bc731edd0dfe25539c033f745bd8126d8a8505
= <=>
2023-12-22 10:54:03 +0530

    added new car

--- cars/BaseCar.gd
+++ cars/BaseCar.gd
@@ -6,6 +6,7 @@ extends VehicleBody3D
 var steer_target = 0
 @export var engine_force_value = 40
 
+
 func _physics_process(delta):
 	var speed = linear_velocity.length()*Engine.get_frames_per_second()*delta
 	traction(speed)
