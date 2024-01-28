7284b965ef190600e20ed1ea19c67d5c27a92351
Weasel On A Stick <korotkih.00@gmail.com>
2024-01-28 17:07:15 +0700

    initial commit

--- cars/BaseCar.gd
+++ cars/BaseCar.gd
@@ -6,50 +6,54 @@ extends VehicleBody3D
 var steer_target = 0
 @export var engine_force_value = 40
 
+var fwd_mps : float
+var speed: float
+
 
 func _physics_process(delta):
-	var speed = linear_velocity.length()*Engine.get_frames_per_second()*delta
+	speed = linear_velocity.length()*Engine.get_frames_per_second()*delta
+	fwd_mps = transform.basis.x.x
 	traction(speed)
+	process_accel(delta)
+	process_steer(delta)
+	process_brake(delta)
 	$Hud/speed.text=str(round(speed*3.8))+"  KMPH"
 
-	var fwd_mps = transform.basis.x.x
-	steer_target = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
-	steer_target *= STEER_LIMIT
-	if Input.is_action_pressed("ui_down"):
-	# Increase engine force at low speeds to make the initial acceleration faster.
-
-		if speed < 20 and speed != 0:
-			engine_force = clamp(engine_force_value * 3 / speed, 0, 300)
-		else:
-			engine_force = engine_force_value
-	else:
-		engine_force = 0
+func process_accel(delta):	
 	if Input.is_action_pressed("ui_up"):
 		# Increase engine force at low speeds to make the initial acceleration faster.
 		if fwd_mps >= -1:
 			if speed < 30 and speed != 0:
-				engine_force = -clamp(engine_force_value * 10 / speed, 0, 300)
+				engine_force = clamp(engine_force_value * 10 / speed, 0, 300)
 			else:
-				engine_force = -engine_force_value
+				engine_force = engine_force_value		
+		return
+	
+	if Input.is_action_pressed("ui_down"):
+	# Increase engine force at low speeds to make the initial acceleration faster.
+		if speed < 20 and speed != 0:
+			engine_force = -clamp(engine_force_value * 3 / speed, 0, 300)
 		else:
-			brake = 1
-	else:
-		brake = 0.0
-		
-	if Input.is_action_pressed("ui_select"):
-		brake=3
-		$wheal2.wheel_friction_slip=0.8
-		$wheal3.wheel_friction_slip=0.8
-	else:
-		$wheal2.wheel_friction_slip=3
-		$wheal3.wheel_friction_slip=3
-	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
+			engine_force = -engine_force_value
+		return
+	
+	engine_force = 0
+	brake = 0	
 
+func process_steer(delta):
+	steer_target = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	steer_target *= STEER_LIMIT
+
+func process_brake(delta):
+	if Input.is_action_pressed("ui_select"):
+		brake=0.5
+		$wheel_rear_left.wheel_friction_slip=2
+		$wheel_rear_right.wheel_friction_slip=2
+	else:
+		$wheel_rear_left.wheel_friction_slip=2.9
+		$wheel_rear_right.wheel_friction_slip=2.9
+	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
 
 
 func traction(speed):
 	apply_central_force(Vector3.DOWN*speed)
-
-
-
-
